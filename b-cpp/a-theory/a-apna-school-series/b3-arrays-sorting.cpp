1c20d652f743442c72ad390a290972de50768c33
ravi.prakash <ravi.0531.rp@gmail.com>
2022-12-14 16:52:06 +0530

    Sorting Added Till Insertion sort

--- b-cpp/a-theory/a-apna-school-series/b3-arrays-sorting.cpp
+++ b-cpp/a-theory/a-apna-school-series/b3-arrays-sorting.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-
+// Take the minimum and swap with first repeatedly; and move ahead & repeat
 void selection_sort(int arr[], int length){
     for (int i= 0; i< length - 1; i++){
         for (int j= i + 1; j< length; j++){
@@ -17,10 +17,13 @@ void selection_sort(int arr[], int length){
         
     }
     for (int i=0; i< length; i++){
-        cout << arr[i] << endl;
+        cout << arr[i] << " ";
     }
+
+    cout <<  endl;
 }
 
+// swap if elem on right is less than left throughout the array ; repeat n times
 void bubble_sort(int arr[], int length){
     for (int i=0; i< length-1; i++){
         for (int j= 1; j< length - i; j++){
@@ -32,8 +35,46 @@ void bubble_sort(int arr[], int length){
         }
     }
     for (int i=0; i< length; i++){
-        cout << arr[i] << endl;
+        cout << arr[i] << " ";
     }
+    cout <<  endl;
+}
+
+void bubble_sort2(int arr[], int length){
+    int counter = 0;
+    while (counter < length){
+        for (int i = 0; i < length - 1; i++){
+            if (arr[i] > arr[i+1]){
+                int temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        counter += 1;
+    }
+
+    for (int i=0; i< length; i++){
+        cout << arr[i] << " ";
+    }
+    cout <<  endl;
+}
+
+// Insert an element from an Unsorted Array to its correct position in sorted array
+void insertion_sort(int arr[], int length){
+    for (int i=1; i<length ; i++){
+        int current = arr[i];
+        int tmp = i-1;
+        while (tmp >= 0 && arr[tmp] > current){
+            arr[tmp + 1] = arr[tmp];
+            tmp--;
+        }
+        arr[tmp + 1] = current;
+    }
+
+    for (int i=0; i< length; i++){
+        cout << arr[i] << " ";
+    }
+    cout <<  endl;
 }
 
 int main(){
@@ -49,6 +90,8 @@ int main(){
 
     selection_sort(arr, length);
     bubble_sort(arr, length);
+    bubble_sort2(arr, length);
+    insertion_sort(arr, length);
     return 0;
 
 }
