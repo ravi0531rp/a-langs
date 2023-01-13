56482c086e419df0f19664afeb9f101bbe4f7db3
ravi.prakash <ravi.0531.rp@gmail.com>
2023-01-14 00:26:40 +0530

    Longest Common Subsequence

--- a-python/c-dsa/a-dynamic-prog-csdojo/a3-longest-common-subsequence.py
+++ a-python/c-dsa/a-dynamic-prog-csdojo/a3-longest-common-subsequence.py
@@ -6,16 +6,21 @@ def longest_common_subsequence(str1, str2):
     else:
         return max(longest_common_subsequence(str1[:-1], str2), longest_common_subsequence(str1, str2[:-1]))
 
-def longest_common_substring(str1, str2, counter): # this may be wrong..
-    tmp = counter
-    if len(str1) == 0 or len(str2) == 0:
-        return 0
-    elif str1[-1] == str2[-1]:
-        counter =  1 + longest_common_substring(str1[:-1], str2[:-1],tmp + 1 )
-    else:
-        counter =  max(tmp, longest_common_substring(str1[:-1], str2, 0), longest_common_substring(str1, str2[:-1], 0))
 
-    return counter
+def longest_common_subseq(str1,str2,m,n,memo):
+    if m == 0 or n == 0:
+        memo[m][n] = 0
+        return 0
+    if memo[m][n]:
+        return memo[m][n]
+    
+    if str1[m-1] == str2[n-1]:
+        val = 1 + longest_common_subseq(str1,str2,m-1,n-1,memo)
+        memo[m][n] = val
+    else:
+        val = max(longest_common_subseq(str1,str2,m-1,n, memo), longest_common_subseq(str1,str2,m,n-1,memo))
+        memo[m][n] = val
+    return val
 
 
 
@@ -23,5 +28,9 @@ if __name__ =="__main__":
     string_1 = "backus"
     string_2 = "backsp"
     print(longest_common_subsequence(string_1, string_2))
-    print(longest_common_substring(string_1, string_2,0))   
 
+    m = len(string_1)
+    n = len(string_2)
+
+    memo = [[None]*(m+1)]*(n+1)
+    print(longest_common_subseq(string_1,string_2,m,n,memo))
