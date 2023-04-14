6b93ad9540c77a93f7bbf34fbc4c2fd5813b97d7
ravi.prakash <ravi.0531.rp@gmail.com>
2023-04-15 00:27:41 +0530

    Mass DUMP

---
+++ a-python/f-coding-practices/a-type-hinting.py
@@ -0,0 +1,14 @@
+import math
+
+
+def myfunc(param: int):
+    """Custom Func
+
+    Args:
+        param (int): Integer Value
+    """
+    print(param)
+
+
+if __name__ == "__main__":
+    myfunc("Hello") # if u run mypy filename.py ; it will give errors
